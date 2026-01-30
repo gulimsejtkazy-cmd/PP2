51076f7072384837bf09e90141f94d78693e4523
gulimsejtkazy-cmd <gulimsejtkazy@gmail.com>
2026-01-30 15:06:56 +0600

    variables

--- practice_1/python_basics/variables.py
+++ practice_1/python_basics/variables.py
@@ -8,15 +8,22 @@ x = 5
 y = "John"
 print(x)
 print(y)
+
 # Example 3
-x = 4       # x is of type int
-x = "Sally" # x is now of type str
+x = y = z = "Orange"
 print(x)
+print(y)
+print(z)
+
 # Example 4
 x = str(3)    # x will be '3'
 y = int(3)    # y will be 3
 z = float(3)  # z will be 3.0
+
 # Example 5
-x = "John"
-# is the same as
-x = 'John'
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
