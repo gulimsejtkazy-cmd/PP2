e8e2596a3a10ccc57dd8fc7a9843ed78622d853b
gulimsejtkazy-cmd <gulimsejtkazy@gmail.com>
2026-01-30 15:03:46 +0600

    syntax

--- practice_1/python_basics/data_types.py
+++ practice_1/python_basics/data_types.py
@@ -5,16 +5,19 @@ x = 12
 y = "John"
 print(type(x))
 print(type(y))
+
 # Example 3
 x = 1j
 y = 20.5
 print(type(x))
 print(type(y))
+
 # Example 4
 x = ["apple", "banana", "cherry"]
 y = ("apple", "banana", "cherry")
 print(type(x))
 print(type(y))
+
 # Example 5
 x = {"name" : "John", "age" : 36}
 y = True
--- practice_1/python_basics/syntax.py
+++ practice_1/python_basics/syntax.py
@@ -6,11 +6,14 @@ if 5 > 2:
  print("Five is greater than two!") 
 if 5 > 2:
         print("Five is greater than two!") 
+
 # Example 3
 print("Python is fun!")
+
 # Example 4
 print("Hello World!")
 print("Have a good day.")
 print("Learning Python is fun!")
+
 # Example 5
 print("Hello"); print("How are you?"); print("Bye bye!")
