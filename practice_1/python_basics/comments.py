57d2b58c973675a098d343ba6f7af19bfc199bad
gulimsejtkazy-cmd <gulimsejtkazy@gmail.com>
2026-01-30 14:37:13 +0600

    comments

--- practice_1/python_basics/comments.py
+++ practice_1/python_basics/comments.py
@@ -4,3 +4,20 @@ written in
 more than just one line
 """
 print("Hello, World!")
+
+#Example 2
+#This is a comment
+print("Hello, World!")
+
+#Example 3
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#Example 4
+print("Hello, World!") #This is a comment
+
+#Example 5
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
--- practice_1/python_basics/syntax.py
+++ practice_1/python_basics/syntax.py
@@ -1,2 +1,16 @@
 if 5 > 2:
   print("Five is greater than two!")
+
+# Example 2
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+print("Python is fun!")
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
