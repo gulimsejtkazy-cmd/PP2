ab93e51db59f05e6b9c34b352730b235109335f5
gulimsejtkazy-cmd <gulimsejtkazy@gmail.com>
2026-01-30 14:58:01 +0600

    string

--- practice_1/python_basics/string.py
+++ practice_1/python_basics/string.py
@@ -1,9 +1,11 @@
 a = "Hello, World!"
 print(len(a))
+
 # Example 2
 print("It's alright")
 print("He is called 'Johnny'")
 print('He is called "Johnny"')
+
 # Example 3
 a = """Lorem ipsum dolor sit amet,
 consectetur adipiscing elit,
@@ -14,6 +16,7 @@ print(a)
 # Example 4
 a = "Hello, World!"
 print(a[1])
+
 # Example 5
 for x in "banana":
   print(x)
