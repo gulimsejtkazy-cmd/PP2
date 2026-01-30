58fcbde3ca118ba2bac868ef1825e6d036c5609f
gulimsejtkazy-cmd <gulimsejtkazy@gmail.com>
2026-01-30 13:56:15 +0600

    get started

--- practice_1/python_basics/home.py
+++ practice_1/python_basics/home.py
@@ -1,9 +1,1 @@
-count = 1
-where = input ("go left or right?")
-while where == "right" and count != 2:
-    where = input ("go left or right?")
-    count+=1
-    if(count==2):
-        print("sad face")
-    else:
-        print("You got out!")
+print("Hello, World!")
