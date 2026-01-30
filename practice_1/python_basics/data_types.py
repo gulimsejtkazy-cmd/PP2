41f9913ef08a0e538fc278b861a12a1cc9db7689
gulimsejtkazy-cmd <gulimsejtkazy@gmail.com>
2026-01-30 14:46:24 +0600

    data types

--- practice_1/python_basics/data_types.py
+++ practice_1/python_basics/data_types.py
@@ -1,2 +1,22 @@
 x = 5
 print(type(x))
+# Example 2
+x = 12
+y = "John"
+print(type(x))
+print(type(y))
+# Example 3
+x = 1j
+y = 20.5
+print(type(x))
+print(type(y))
+# Example 4
+x = ["apple", "banana", "cherry"]
+y = ("apple", "banana", "cherry")
+print(type(x))
+print(type(y))
+# Example 5
+x = {"name" : "John", "age" : 36}
+y = True
+print(type(x))
+print(type(y))
