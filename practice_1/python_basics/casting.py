422697aec6cddc2c615aae8b174dbad9973f0a17
gulimsejtkazy-cmd <gulimsejtkazy@gmail.com>
2026-01-30 13:37:27 +0600

    Gulim

--- practice_1/python_basics/casting.py
+++ practice_1/python_basics/casting.py
@@ -1,3 +1,3 @@
-x = str("s1") # x will be 's1'
-y = str(2)    # y will be '2'
-z = str(3.0)  # z will be '3.0'
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
--- practice_1/python_basics/practica.py
+++
@@ -1,7 +0,0 @@
-mysum = 0
-a= int(input())
-b=int(input())
-for i in range (a,b):
-    if (i%2==0):
-        mysum +=1
-print(mysum)
