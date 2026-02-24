30deccfc7ca2f1a8bd64ed2585f1a9d198cee9c9
gulimsejtkazy-cmd <gulimsejtkazy@gmail.com>
2026-02-24 14:18:15 +0600

    generators

--- exe.py
+++ exe.py
@@ -1,1 +1,6 @@
-import datetime
+def g(n):
+    for i in range(n,0,-1):
+        yield i
+n=int(input())
+for j in g(n):
+    print(j)
--- practice_4/generators.py
+++ practice_4/generators.py
@@ -1,14 +1,21 @@
 # 1. Generator of squares from 1 to a
-a = int(input()) 
-g = (i**2 for i in range(1, a+1)) 
-print(*g)  
+def squares(N):
+    for i in range(N + 1):
+        yield i * i
+
+n = int(input())
+
+for num in squares(n):
+    print(num) 
 
 # 2. Generator of even numbers from 0 to n-1
-n = int(input()) 
-g = (i for i in range(n) if i % 2 == 0)  # Generator: all i from 0 to n-1 where i is even
-print(*g, sep=",")  
-
-
+def g(n):
+    for i in range(n):
+        if i%2==0:
+            yield i
+a= int(input())
+for j in g(a):
+    print(j)
 # 3. 
 def f(n):
     for i in range(n):
@@ -20,12 +27,18 @@ for j in f(a):  # Iterate through the generator
     print(j) 
 
 # 4. 
-a = int(input()) 
-b = int(input()) 
-g = (i**2 for i in range(a, b))  # Generator expression: squares of numbers from a to b-1
-print(*g) 
+def g(a,b):
+    for i in range(a,b):
+        yield i**2
+a= int(input())
+b=int(input())
+for j in g(a,b):
+    print(j)
 
 # 5. 
-n = int(input())  
-g = (i for i in range(n, 0, -1))  # Generator: numbers from n down to 1 (step -1)
-print(*g) 
+def g(n):
+    for i in range(n,0,-1):
+        yield i
+n=int(input())
+for j in g(n):
+    print(j)
