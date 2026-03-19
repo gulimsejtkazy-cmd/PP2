acf25cfe3614c7a9b9dc41dcde368c69c8681fca
gulimsejtkazy-cmd <gulimsejtkazy@gmail.com>
2026-03-19 21:48:16 +0600

    Update map_filter_reduce.py

--- practice_6/file_handling/map_filter_reduce.py
+++ practice_6/file_handling/map_filter_reduce.py
@@ -5,16 +5,19 @@ nums = [1, 2, 3, 4, 5]
 
 # map
 squared = list(map(lambda x: x**2, nums))
+print("Squares:", squared)
+#applies a function to all elements
 
 # filter
 even = list(filter(lambda x: x % 2 == 0, nums))
+print("Even:", even)
+# leaves only elements where the condition is True
 
 # reduce
 sum_all = reduce(lambda x, y: x + y, nums)
 
-print(squared)
-print(even)
 print(sum_all)
+# reduce takes a list and “compresses” it into a single value
 # 2
 from functools import reduce
 
