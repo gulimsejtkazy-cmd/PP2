4198724221a217f2bd2f5f42a49365804ee25961
gulimsejtkazy-cmd <gulimsejtkazy@gmail.com>
2026-03-19 21:52:40 +0600

    Update move_files.py

--- practice_6/directory_management/move_files.py
+++ practice_6/directory_management/move_files.py
@@ -1,9 +1,10 @@
 # 1
 import shutil
-
+# move file
 shutil.move("sample.txt", "new_folder/sample.txt")
+# disappears from the old place
+# appears in a new one
+# Moves the file example.txt to the folder folder1
 
-# 2
-import shutil
-
-shutil.move("sample.txt", "new_folder/sample.txt")
+# Copy back
+shutil.copy("folder1/example.txt", "example_copy.txt")
